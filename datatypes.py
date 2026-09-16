a69bba438c6397019f19e4fb498c15a351d6d6ec
Kevin Daniel <kevinkunnathu03@gmail.com>
2026-09-16 16:00:35 +0530

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,41 @@
+# operations
+# int
+# float
+# operations
+# arithemetic operations
+# addition +
+
+a = 9
+b = 4
+
+print(a+b)
+
+# subtraction -
+
+print(a-b)
+
+# multiplication *
+print(a*b)
+
+# division /
+print(a/b)
+
+# floor division //
+print(a//b)
+
+# exponent **
+print(a**b)
+
+# modules %
+print(a % b)
+
+# string=sequence of series of char
+
+name = "kevin"
+print(name)
+name = "manav's birthday"
+quote = '''gandhi says"Be happy in your life" '''
+phone_number = "8157065241"
+print(name)
+print(quote)
+print(phone_number)
---
+++ info.py
@@ -0,0 +1,21 @@
+# print("kevin Daniel")
+a = 20
+b = 10
+print(a, b)
+age = 23
+print(age)
+a = 5
+b = 7
+temp = a
+a = b
+b = temp
+print(a, b)
+a = 5
+b = 7
+print(a+2, b-2)
+a = 5
+b = 7
+a = a+b
+b = a-b
+a = a-b
+print(a, b)
