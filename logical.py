4dcf553df14d0ad0564d1fc880d86229ab43ff4a
Kevin Daniel <kevinkunnathu03@gmail.com>
2026-09-18 22:36:01 +0530

    Add files via upload

---
+++ logical.py
@@ -0,0 +1,23 @@
+# elif
+
+# smark = 90
+# if smark > 90:
+#     print("A")
+# elif smark > 80:
+#     print("B")
+# elif smark > 70:
+#     print("C")
+# elif smark > 60:
+#     print("D")
+# else:
+#     print("fail")
+
+# check if a number is positive negative or zero
+
+# num = int(input("Enter a number: "))
+# if num > 0:
+#     print("The number is positive.")
+# elif num < 0:
+#     print("The number is negative.")
+# else:
+#     print("The number is zero.")
---
+++ operator.py
@@ -0,0 +1,34 @@
+# logical -- OR, AND , NOT
+
+#or
+
+# a=12
+# b=3
+# if a>10 or b==0:
+#     print("yes")
+
+#and
+ 
+# a=15
+# b=4
+# if a==15 and b % 3==0:
+#     print("ok")
+
+#not
+
+# a=17
+# if not a>18:
+#     print("set")
+
+
+#largest of 3 numbers
+
+# num1=int(input("enter the number"))
+# num2=int(input("enter the number"))
+# num3=int (input("enter the number"))
+# if num1>num2 and num1>num3:
+#         print("num1 is largest")
+# elif num2>num1 and num2>num3:
+#         print("num2 is largest")
+# else:
+#         print("num3 is largest")                
